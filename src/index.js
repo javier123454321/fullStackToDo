2b31b8cf1e22c7b23bd3e7f7f74b69bcd5e64e2b
Javier Gonzalez <Hal@Dr-Jeckyll.lan>
2020-03-28 15:04:26 -0500

    render through vue

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
+let app = new Vue({
+    el: '#root',
+    data:{
+        isLoaded: false,
+        tasks: [],
+        },
 
-let tasks;
-let xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-        tasks = JSON.parse(this.responseText);
-        console.log(tasks);
-        
-    for(i in tasks){        
-        let strikeThrough = "";
-        console.log(tasks[i].completed);
-        
-        if(tasks[i].completed == "1"){
-            strikeThrough = "cross"
+    methods:{ 
+    },
+    mounted() {
+        fetch(`../controllers/taskFunctions/getTask.php `)
+            .then((response) => response.json())
+            .then((data) => {
+                this.tasks = data;
+                this.isLoaded = true;
+            console.log('Success:', data);
+            })
         }
-        document.getElementById("taskList").innerHTML += `
-                <li id=${i} class="list-group-item ${strikeThrough}"> 
-                    ${tasks[i].description} 
-                </li>`;
-    }
-  }
-};
-xmlhttp.open("GET", "../controllers/taskFunctions/getTask.php", true);
-xmlhttp.send();
+})
 
+  
--- views/index.view.php
+++ views/index.view.php
@@ -1,12 +1,18 @@
 <? require (__DIR__.'/partialviews/header'); ?>
 
 
-<div class="container d-flex justify-content-center mt-5">
+<div id="root" class="container d-flex justify-content-center mt-5">
     <div class="col-lg-5 col-sm-10 card">
         <h3 class="card-header">To Do Items:</h3>
-        <ul id="taskList" class="list-group list-group-flush">
-            
+        <ul id="taskList" v-if="this.isLoaded" class="list-group list-group-flush">
+           <li v-for="task in tasks" :bind="tasks" 
+                class="list-group-item"
+                :class="{ cross: (task.completed == '1') }">
+               {{ task.description }} 
         </ul>
+        <div v-else class="header">
+            <p>loading...</p>
+        </div>
         <div class="p-3 form-group">
             <form method="POST" action="/../controllers/taskFunctions/updateTask.php" class="row justify-content-between">
                 <input class="form-text p-2 col-lg-8 col-md-6" type="text" name="newTask" id="newTask" placeholder="Write a new task...">
@@ -15,7 +21,3 @@
         </div>
     </div> 
 </div> 
-
-</body>
-<script src="../src/index.js"></script>
-</html>
